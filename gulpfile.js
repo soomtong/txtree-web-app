1347b6a6e984d28b21a7ba70e2c1c0d47239c6fd
soomtong <soomtong@gmail.com>
2015-10-21 10:31:32 +0900

    add less build script for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
+var rename = require("gulp-rename");
 var source = require('vinyl-source-stream');
 
 var browserify = require('browserify');
 var babelify = require('babelify');
+var less = require('gulp-less');
 
 // Without watchify
 gulp.task('browserify', function () {
@@ -11,6 +13,13 @@ gulp.task('browserify', function () {
         .bundle()
         .pipe(source('build.js'))
         .pipe(gulp.dest('script'));
+});
+
+gulp.task('stylesheet', function () {
+    gulp.src('style/main.less')
+        .pipe(less())
+        .pipe(rename('build.css'))
+        .pipe(gulp.dest('style'));
 });
 
 gulp.task('default', ['browserify']);
