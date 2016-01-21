77303ba677cc4f5bba3c05c0b303bcf33f1c4de8
soomtong <soomtong@gmail.com>
2016-01-21 17:21:25 +0900

    updated bookmarklet

--- view/bookmarklet.js
+++ view/bookmarklet.js
@@ -1,27 +1,26 @@
-var q = "" + window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text;
-var l = location.href;
-if (!q || q.type != 'Range') {
-    alert("You didn't select any text. Select phrase to Save for Txtree");
-} else {
-    var doc = ''.concat('\n[', l, '](', l, ')\n', '\n```\n', q, '\n```\n');
-    var httpRequest;
-    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-        httpRequest = new XMLHttpRequest();
-    } else if (window.ActiveXObject) { // IE
-        try {
-            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-        }
-        catch (e) {
-            try {
-                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-            }
+var txtreeEmbed = {
+    element: 'txtree_embed',
+    location: location.href,
+    selection: "" + window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text,
+    httpRequest: null,
+    getHttpRequest: function () {
+        if (window.XMLHttpRequest) this.httpRequest = new XMLHttpRequest();
+        else if (window.ActiveXObject) { // IE
+            try { this.httpRequest = new ActiveXObject("Msxml2.XMLHTTP"); }
             catch (e) {
+                try { this.httpRequest = new ActiveXObject("Microsoft.XMLHTTP"); }
+                catch (e) { }
             }
         }
-    }
-    if (!httpRequest) {
-        alert('Giving up :( Cannot create an XMLHTTP instance');
-    } else {
+
+        return this;
+    },
+    postHttpRequest: function () {
+        var httpRequest = this.httpRequest;
+        if (!httpRequest) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return;
+        }
         httpRequest.onReadyStateChange = function () {
             if (httpRequest.readyState === 4) {
                 if (httpRequest.status === 200) {
@@ -35,6 +34,22 @@ if (!q || q.type != 'Range') {
         httpRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         httpRequest.setRequestHeader("X-Access-Host", "txtree-bookmarklet");
         httpRequest.send(JSON.stringify({ theme: 'markdown/github', text: doc }));
+
+        return this;
+    },
+    closeEmbed: function () {
+        document.getElementById(this.element).remove();
     }
+}; if (txtreeEmbed.selection && txtreeEmbed.selection.type == 'Range') {
+    var textArray = [
+        '[' + txtreeEmbed.location + '](' + txtreeEmbed.location + ')',
+        '```',
+        txtreeEmbed.selection,
+        '```'
+    ];
+    var doc = textArray.join('\n');
+    txtreeEmbed.getHttpRequest().postHttpRequest().closeEmbed();
+} else {
+    alert("You didn't select any text. Select phrase to Save for Txtree");
+    txtreeEmbed.closeEmbed();
 }
-document.getElementById('txtree_embed').remove();
